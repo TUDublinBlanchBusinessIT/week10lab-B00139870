0bfd08b91acc13ee6bb5c71892d161276f4aeee5
B00139870 Komal Wajahat <B00139870@mytudublin.ie>
2023-11-22 09:56:45 +0000

    part 1 and 2 done

--- processForm1.php
+++ processForm1.php
@@ -2,27 +2,27 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Gathering form data
-    $firstname = $_POST['firstname'] ?? '';
-    $surname = $_POST['surname'] ?? '';
+// form data from flight?Booking.html 
+    $firstname = $_POST['firstname'] ;
+    $surname = $_POST['surname'] ;
     $luggage = isset($_POST['luggage']) ? 1 : 0;
 
-    // Storing data in session variables
+   // storing 
     $_SESSION['passengerFN'] = $firstname;
     $_SESSION['passengerSN'] = $surname;
     $_SESSION['luggage'] = $luggage;
 
     if ($luggage == 1) {
-        // If luggage checkbox is checked, redirect to luggage.html
+     
         header("Location: luggage.html");
         exit();
     } else {
-        // If luggage checkbox is not checked, redirect to finalStep.php
+
         header("Location: finalStep.php");
         exit();
     }
 } else {
-    // Redirect to the form if accessed without POST method
+
     header("Location: flightBooking.html");
     exit();
 }
